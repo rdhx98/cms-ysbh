abbe114f54d50cb17cb53122d0594e8a38f9bf78
rdhx98 <roi.liem98@gmail.com>
2026-07-29 22:14:36 +0900

    to laptop at 29/7/26 10:14

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -12,5 +12,12 @@ class Page extends Model
     use HasFactory;
 
     // Field yang boleh diisi massal
-    protected $fillable = ['title', 'slug', 'content', 'status', 'created_at', 'updated_at'];
+    protected $fillable = ['title', 'slug', 'content', 'status', 'meta_title','meta_description', 'published_at', 'created_at', 'updated_at'];
+    public $translatable = ['title', 'content', 'meta_title', 'meta_description'];
+    protected $casts = [  'published_at' => 'datetime', ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- database/migrations/2026_05_28_141619_c_m_sdb_migration.php
+++ database/migrations/2026_05_28_141619_c_m_sdb_migration.php
@@ -33,6 +33,8 @@ return new class extends Migration
             $table->string('title')->unique();
             $table->string('slug')->unique();
             $table->longText('content');
+            $table->json('meta_title')->nullable();
+            $table->json('meta_description')->nullable();
             $table->string('featured_image')->nullable(); // Jalur file di FTP Hostinger
             $table->enum('status', ['draft', 'review', 'published', 'scheduled', 'archived', 'rejected'])->default('draft');
             $table->timestamp('published_at')->nullable();
@@ -52,14 +54,19 @@ return new class extends Migration
             // Anda bisa tambah kolom 'is_primary' jika ingin menandai penulis utama
             $table->boolean('is_primary')->default(false);
         });
+
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
             $table->string('slug')->unique();
             $table->longText('content');
-            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->enum('status', ['online', 'offline'])->default('offline');
+            $table->json('meta_title')->nullable();
+            $table->json('meta_description')->nullable();
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
+
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->string('label')->unique();
